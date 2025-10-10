bded8b2ef9e7dec1acc173d224ffb738ae76adbf
Elameen Daiyabu <elameendaiyabu@proton.me>
2025-10-10 09:25:56 +0100

    added get user util

--- src/actions/auth/dal.ts
+++ src/actions/auth/dal.ts
@@ -3,6 +3,9 @@ import { headers } from "next/headers";
 import { cache } from "react";
 import { redirect } from "next/navigation";
 import { auth } from "@/lib/auth";
+import { db } from "@/db";
+import { employees } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export const verifySession = cache(async () => {
   const session = await auth.api.getSession({ headers: await headers() });
@@ -12,4 +15,25 @@ export const verifySession = cache(async () => {
   }
 
   return { isAuth: true, userId: session.user.id, role: session.user.role };
+});
+
+export const getUser = cache(async () => {
+  const session = await verifySession();
+  if (!session.userId) return null;
+
+  const [user] = await db
+    .select({
+      id: employees.id,
+      staffNumber: employees.staffNumber,
+      name: employees.name,
+      role: employees.role,
+      department: employees.department,
+      managerId: employees.managerId,
+      isManager: employees.isManager,
+    })
+    .from(employees)
+    .where(eq(employees.authId, session.userId))
+    .limit(1);
+
+  return user;
 });
